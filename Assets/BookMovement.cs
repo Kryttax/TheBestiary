36c39e42e048e189373d6da83839cd11b93a65dc
unknown <kryttax@gmail.com>
2019-08-21 13:58:07 +0200

    Initial Commit: Basic vestion of Bestiary included. You can navigate trough pages and add new ones. Works with Win64 builds.

---
+++ Assets/BookMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookMovement : MonoBehaviour
+{
+    public GameObject bookRef;
+    AutoFlip flip;
+    
+    private void Awake()
+    {
+        
+        flip = bookRef.GetComponent<AutoFlip>();       
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+            Application.Quit();
+
+            //FlipPages();
+    }
+
+    void FlipPages()
+    {
+    
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            flip.FlipRightPage();
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftArrow))
+            flip.FlipLeftPage();
+    }
+}
